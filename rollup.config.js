df4c7bc0ff9acd3175c1231aee5c23edc6618687
knowledgecode <knowledgecode@gmail.com>
2021-10-17 22:01:45 +0900

    Organized the modules to build.

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,14 @@ export default [
     {
         input: 'src/index.js',
         output: [
-            { file: 'esm/delegate.es.js', format: 'es' },
             { file: 'delegate.js', format: 'umd', name: 'delegate', esModule: false }
         ]
     },
     {
         input: 'src/index.js',
         output: [
-            { file: 'esm/delegate.es.min.js', format: 'es' },
-            { file: 'delegate.min.js', format: 'umd', name: 'date', esModule: false }
+            { file: 'esm/delegate.js', format: 'es' },
+            { file: 'delegate.min.js', format: 'umd', name: 'delegate', esModule: false }
         ],
         plugins: [compiler()]
     }
