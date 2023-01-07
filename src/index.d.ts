81217edc08a180f21f7ec4ad9aeeef7d47a9df6c
knowledgecode <knowledgecode@gmail.com>
2023-01-07 10:35:15 +0900

    TypeScript support

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+declare class DelegateEvent {
+    constructor(evt: Event, target: EventTarget);
+    preventDefault(): void;
+    stopPropagation(): void;
+    stopImmediatePropagation(): void;
+    originalEvent: Event;
+    currentTarget: EventTarget;
+}
+
+declare class Delegate {
+    constructor(baseEventTarget: EventTarget);
+    on(eventName: string, selector: string, handler: (evt: DelegateEvent) => void): Delegate;
+    on(eventName: string, handler: (evt: DelegateEvent) => void): Delegate;
+    one(eventName: string, selector: string, handler: (evt: DelegateEvent) => void): Delegate;
+    one(eventName: string, handler: (evt: DelegateEvent) => void): Delegate;
+    off(eventName: string, selector: string, handler: (evt: DelegateEvent) => void): Delegate;
+    off(eventName: string, handler: (evt: DelegateEvent) => void): Delegate;
+    off(eventName: string, selector: string): Delegate;
+    off(eventName: string): Delegate;
+    off(): Delegate;
+    clear(): void
+}
+
+export default function (baseEventTarget: EventTarget): Delegate;
